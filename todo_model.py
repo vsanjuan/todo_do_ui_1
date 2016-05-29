084fd82162a5eff9ffaea956ec28248940a64e7f
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-29 09:20:02 +0200

    feat hiearchical model tag field

--- todo_model.py
+++ todo_model.py
@@ -4,8 +4,15 @@ from openerp import models, fields, api
 class Tag(models.Model):
 	_name = 'todo.task.tag'
 	name = fields.Char('Name', 40 , translate=True)
-	# Relaltions
+	# Relations
 	task_ids = fields.Many2many('todo.task','Task')
+	# Hierarchical relations
+	_parent_store = True
+	# _parent_name = 'parent_id'
+	parent_id = fields.Many2one('todo.task.tag','Parent Tag',ondelete='restrict')
+	parent_left = fields.Integer('Parent Left',index=True)
+	parent_right = fields.Integer('Parent Right', index=True)
+	child_ids = fields.One2many('todo.task.tag','parent_id','Child Tags')
 
 class Stage(models.Model):
 	_name = 'todo.task.stage'
